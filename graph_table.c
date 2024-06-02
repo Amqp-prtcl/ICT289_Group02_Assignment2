d5e93ced33dbe9419d537ea7ff213e274e1d4535
Alex <bochurbergalex@gmail.com>
2024-06-02 11:38:37 +0800

    Graph table update

--- graph_table.c
+++ graph_table.c
@@ -5,6 +5,7 @@ enum graph_type {
     RECTANGLE,
     POLYGON,
     BEZIER3,
+    B,
 };
 
 struct graph_wall {
@@ -16,22 +17,330 @@ struct graph_wall {
     Vector3 p4;
 };
 
+// {0.5, 0.25, 0}, --> Brown
+
 static struct graph_wall walls[] = {
+        // Table ground
     {
-        {1, 1, 1},
-        BEZIER3,
-        {0, 0, 0},
         {0, 1, 0},
-        {0, 1, 1},
-        {0.2, 1, 1,},
+        RECTANGLE,
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
     },
+        // Table INSIDE sides
+    // Side width
+    {
+        {0, 0, 1},
+        RECTANGLE,
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS/2, 0, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS/2, 0, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+    },
+    {
+        {1, 0, 1},
+        RECTANGLE,
+        {PLAYFIELD_LENGTH/2, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, TABLE_HEIGHT, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+    },
+    // Side length
+    // 1/2
+    {
+        {0, 0, 1},
+        RECTANGLE,
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+        {HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+        {HOLE_RADIUS, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2},
+    },
+    {
+        {1, 0, 1},
+        RECTANGLE,
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2},
+        {HOLE_RADIUS, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2},
+        {HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2},
+    },
+    // 2/2
+        {
+        {0, 0, 1},
+        RECTANGLE,
+        {-HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2},
+    },
+    {
+        {1, 0, 1},
+        RECTANGLE,
+        {-HOLE_RADIUS, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2},
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2},
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2},
+    },
+    // Side width
+    {
+        {0, 0, 1},
+        RECTANGLE,
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS/2, 0, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS/2, 0, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+    },
+    {
+        {1, 0, 1},
+        RECTANGLE,
+        {-PLAYFIELD_LENGTH/2, TABLE_HEIGHT-0.005, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+    },
+    // Side length
+    // 1/2
+    {
+        {0, 0, 1},
+        RECTANGLE,
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+        {-HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+        {-HOLE_RADIUS, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2},
+    },
+    {
+        {1, 0, 1},
+        RECTANGLE,
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2},
+        {-HOLE_RADIUS, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2},
+        {-HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2},
+    },
+    // 2/2
+        {
+        {0, 0, 1},
+        RECTANGLE,
+        {HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2},
+    },
+    {
+        {1, 0, 1},
+        RECTANGLE,
+        {HOLE_RADIUS, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2},
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT-0.005, -PLAYFIELD_WIDTH/2},
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2},
+    },
+        // Table OUTSIDE sides
+    {
+        {0,1,0},
+        RECTANGLE,
+        {TABLE_LENGTH/2, 0, -TABLE_WIDTH/2+TABLE_HEIGHT},
+        {TABLE_LENGTH/2, 0, TABLE_WIDTH/2-TABLE_HEIGHT},
+        {TABLE_LENGTH/2, TABLE_HEIGHT, TABLE_WIDTH/2-TABLE_HEIGHT},
+    },
+    {
+        {0,1,0},
+        RECTANGLE,
+        {TABLE_LENGTH/2-HOLE_RADIUS, 0, TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2+HOLE_RADIUS, 0, TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, TABLE_WIDTH/2},
+    },
+    {
+        {0,1,0},
+        RECTANGLE,
+        {-TABLE_LENGTH/2, 0, TABLE_WIDTH/2-HOLE_RADIUS},
+        {-TABLE_LENGTH/2, 0, -TABLE_WIDTH/2+HOLE_RADIUS},
+        {-TABLE_LENGTH/2, TABLE_HEIGHT, -TABLE_WIDTH/2+HOLE_RADIUS},
+    },
+    {
+        {0,1,0},
+        RECTANGLE,
+        {-TABLE_LENGTH/2+HOLE_RADIUS, 0, -TABLE_WIDTH/2},
+        {TABLE_LENGTH/2-HOLE_RADIUS, 0, -TABLE_WIDTH/2},
+        {TABLE_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, -TABLE_WIDTH/2},
+    },
+        // Corner table EXTERIOR
+    {
+        {1, 0, 0},
+        BEZIER3,
+        {TABLE_LENGTH/2, 0, TABLE_WIDTH/2-HOLE_RADIUS},
+        {TABLE_LENGTH/2, 0, TABLE_WIDTH/2},
+        {TABLE_LENGTH/2-HOLE_RADIUS, 0, TABLE_WIDTH/2},
+        {TABLE_LENGTH/2-HOLE_RADIUS, -TABLE_HEIGHT, TABLE_WIDTH/2},
+    },
+    {
+        {1, 0, 0},
+        BEZIER3,
+        {-TABLE_LENGTH/2+HOLE_RADIUS, 0, TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2, 0, TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2, 0, TABLE_WIDTH/2-HOLE_RADIUS},
+        {-TABLE_LENGTH/2, -TABLE_HEIGHT, TABLE_WIDTH/2-HOLE_RADIUS},
+    },
+    {
+        {1, 0, 0},
+        BEZIER3,
+        {-TABLE_LENGTH/2, 0, -TABLE_WIDTH/2+HOLE_RADIUS},
+        {-TABLE_LENGTH/2, 0, -TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2+HOLE_RADIUS, 0, -TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2+HOLE_RADIUS, -TABLE_HEIGHT, -TABLE_WIDTH/2},
+    },
+    {
+        {1, 0, 0},
+        BEZIER3,
+        {TABLE_LENGTH/2-HOLE_RADIUS, 0, -TABLE_WIDTH/2},
+        {TABLE_LENGTH/2, 0, -TABLE_WIDTH/2},
+        {TABLE_LENGTH/2, 0, -TABLE_WIDTH/2+HOLE_RADIUS},
+        {TABLE_LENGTH/2, -TABLE_HEIGHT, -TABLE_WIDTH/2+HOLE_RADIUS},
+    },
+        // Corner table INTERIOR
+    {
+        {0.5, 0.5, 0.5},
+        BEZIER3,
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2},
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, -TABLE_HEIGHT, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        BEZIER3,
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2},
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, -TABLE_HEIGHT, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        BEZIER3,
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2},
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, -TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        BEZIER3,
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2},
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, -TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+    },
+        // Corner table LINK
+    {
+        {0.5, 0.5, 0.5},
+        B,
+        {TABLE_LENGTH/2, TABLE_HEIGHT, TABLE_WIDTH/2-HOLE_RADIUS},
+        {TABLE_LENGTH/2, TABLE_HEIGHT, TABLE_WIDTH/2},
+        {TABLE_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, TABLE_WIDTH/2},
+        {PLAYFIELD_LENGTH/2, TABLE_HEIGHT, PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        B,
+        {-TABLE_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2, TABLE_HEIGHT, TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2, TABLE_HEIGHT, TABLE_WIDTH/2-HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        B,
+        {-TABLE_LENGTH/2, TABLE_HEIGHT, -TABLE_WIDTH/2+HOLE_RADIUS},
+        {-TABLE_LENGTH/2, TABLE_HEIGHT, -TABLE_WIDTH/2},
+        {-TABLE_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, -TABLE_WIDTH/2},
+        {-PLAYFIELD_LENGTH/2, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        B,
+        {TABLE_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, -TABLE_WIDTH/2},
+        {TABLE_LENGTH/2, TABLE_HEIGHT, -TABLE_WIDTH/2},
+        {TABLE_LENGTH/2, TABLE_HEIGHT, -TABLE_WIDTH/2+HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2},
+    },
+        // Corner table MIDDLE
+    {
+        {0.5, 0.5, 0.5},
+        BEZIER3,
+        {HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+        {0, 0, PLAYFIELD_WIDTH/2+(TABLE_WIDTH-PLAYFIELD_WIDTH)/2},
+        {-HOLE_RADIUS, 0, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+        {-HOLE_RADIUS, -TABLE_HEIGHT, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        BEZIER3,
+        {HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+        {0, 0, -PLAYFIELD_WIDTH/2-(TABLE_WIDTH-PLAYFIELD_WIDTH)/2},
+        {-HOLE_RADIUS, 0, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+        {-HOLE_RADIUS, -TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+    },
+        // Corner table Ling MIDDLE
+    {
+        {0.5, 0.5, 0.5},
+        B,
+        {HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2+2*HOLE_RADIUS},
+        {0, TABLE_HEIGHT, PLAYFIELD_WIDTH/2+HOLE_RADIUS+(TABLE_WIDTH-PLAYFIELD_WIDTH)/2},
+        {-HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2+2*HOLE_RADIUS},
+        {-HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2+HOLE_RADIUS/2},
+    },
+    {
+        {0.5, 0.5, 0.5},
+        B,
+        {HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-2*HOLE_RADIUS},
+        {0, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-HOLE_RADIUS-(TABLE_WIDTH-PLAYFIELD_WIDTH)/2},
+        {-HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-2*HOLE_RADIUS},
+        {-HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2-HOLE_RADIUS/2},
+    },
+        // Width fill TOP
+    // width
+    {
+        {0.5, 0.25, 0},
+        POLYGON,
+        {TABLE_LENGTH/2, TABLE_HEIGHT, TABLE_WIDTH/2-HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, TABLE_HEIGHT, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {PLAYFIELD_LENGTH/2, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {TABLE_LENGTH/2, TABLE_HEIGHT, -TABLE_WIDTH/2+HOLE_RADIUS},
+    },
+    {
+        {0.5, 0.25, 0},
+        POLYGON,
+        {-TABLE_LENGTH/2, TABLE_HEIGHT, -TABLE_WIDTH/2+HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2+HOLE_RADIUS},
+        {-PLAYFIELD_LENGTH/2, TABLE_HEIGHT, PLAYFIELD_WIDTH/2-HOLE_RADIUS},
+        {-TABLE_LENGTH/2, TABLE_HEIGHT, TABLE_WIDTH/2-HOLE_RADIUS},
+    },
+    // length
+    {
+        {0.5, 0.25, 0},
+        POLYGON,
+        {TABLE_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, TABLE_WIDTH/2},
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2},
+        {HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2},
+        {0, TABLE_HEIGHT, TABLE_WIDTH/2},
+    },
+    {
+        {0.5, 0.25, 0},
+        POLYGON,
+        {-TABLE_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, TABLE_WIDTH/2},
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2},
+        {-HOLE_RADIUS, TABLE_HEIGHT, PLAYFIELD_WIDTH/2},
+        {0, TABLE_HEIGHT, TABLE_WIDTH/2},
+    },
+    {
+        {0.5, 0.25, 0},
+        POLYGON,
+        {TABLE_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, -TABLE_WIDTH/2},
+        {PLAYFIELD_LENGTH/2-HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2},
+        {HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2},
+        {0, TABLE_HEIGHT, -TABLE_WIDTH/2},
+    },
+    {
+        {0.5, 0.25, 0},
+        POLYGON,
+        {-TABLE_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, -TABLE_WIDTH/2},
+        {-PLAYFIELD_LENGTH/2+HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2},
+        {-HOLE_RADIUS, TABLE_HEIGHT, -PLAYFIELD_WIDTH/2},
+        {0, TABLE_HEIGHT, -TABLE_WIDTH/2},
+    }
 };
 
 static void check_graph_wall(struct graph_wall *w) {
     if (w->type != RECTANGLE)
         return;
     Vector3 a;
-    vector3_sub(w->p2, w->p1, a);
+    vector3_sub(w->p1, w->p2, a);
     vector3_add(w->p3, a, w->p4);
 }
 
@@ -66,6 +375,25 @@ static void draw_graph_wall(const struct graph_wall *w) {
             break;
         case BEZIER3:
             vector3_sub(w->p3, w->p4, d);
+            glBegin(GL_QUAD_STRIP);
+            glVertex3fv(w->p1);
+            vector3_add(w->p1, d, v);
+            glVertex3fv(v);
+            for (GLfloat t = 0; t < 1.0; t += 1/BEZIER_PREC) {
+                vector3_lerp(w->p1, w->p2, t, v);
+                vector3_lerp(w->p2, w->p3, t, v1);
+                vector3_lerp(v, v1, t, v);
+                glVertex3fv(v);
+                vector3_add(v, d, v);
+                glVertex3fv(v);
+            }
+            glVertex3fv(w->p3);
+            vector3_add(w->p3, d, v);
+            glVertex3fv(v);
+            glEnd();
+            break;
+        case B:
+            vector3_sub(w->p4, w->p3, d);
             glBegin(GL_QUAD_STRIP);
             glVertex3fv(w->p1);
             vector3_add(w->p1, d, v);
